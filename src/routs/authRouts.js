eeb39aa954cbb7cd0341b62355e3862a97ca88c2
efiVanso <efi.vanso@gmail.com>
2021-01-09 21:40:22 +0200

    Error Handling

--- src/routs/authRouts.js
+++ src/routs/authRouts.js
@@ -1,15 +1,19 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const User = mongoose.model('User')
+const express = require("express");
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
 
 const router = express.Router();
 
-router.post('/signup', async (req,res)=>{
-    const {email, password} = req.body;
-    const user = new User ({email, password});
+router.post("/signup", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = new User({ email, password });
     await user.save();
 
-    res.send('user saved')
+    res.send("user saved");
+  } catch (err) {
+    return res.status(422).send(err.message);
+  }
 });
 
 module.exports = router;
