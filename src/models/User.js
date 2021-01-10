8a8a7164593d11f81fe97a473b93cbc7883904b6
efiVanso <efi.vanso@gmail.com>
2021-01-10 12:19:19 +0200

    Salting and Hashing
    and add signin rout

--- src/models/User.js
+++ src/models/User.js
@@ -1,15 +1,51 @@
-const mongoose = require ('mongoose')
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-    email:{
-        type:String,
-        required:true,
-        unique: true
-    },
-    password:{
-        type:String,
-        required:true
-    }
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
-mongoose.model('User', userSchema)
+userSchema.pre("save", function () {
+    const user = this;
+    if (!user.isModified("password")) {
+        return next();
+    }
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) {
+        return next(err);
+        }
+    });
+
+    bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) {
+        return next(err);
+        }
+        user.password = hash;
+        next();
+    });
+});
+
+userSchema.method.comparePassword = function (candidatePassword) {
+  // check if this the correct pwd on login
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+      if (err) {
+        return reject(err);
+      }
+      if (!isMatch) {
+        return reject(false);
+      }
+      resolve(true);
+    });
+  });
+};
+
+mongoose.model("User", userSchema);
